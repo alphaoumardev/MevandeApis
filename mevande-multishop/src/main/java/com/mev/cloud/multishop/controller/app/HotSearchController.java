eeec00b745050c7bebd5fdbf4e9bc8fe8be20748
Alpha <alphaoumardev@outlook.com>
2024-03-11 23:00:01 -0400

    the leaf model data sync

--- mevande-multishop/src/main/java/com/mev/cloud/multishop/controller/app/HotSearchController.java
+++ mevande-multishop/src/main/java/com/mev/cloud/multishop/controller/app/HotSearchController.java
@@ -22,10 +22,14 @@ import java.util.List;
 @Tag(name = "app-热搜")
 public class HotSearchController
 {
-    @Autowired
-    private HotSearchService hotSearchService;
+    private final HotSearchService hotSearchService;
 
-	@GetMapping("/list")
+    public HotSearchController(HotSearchService hotSearchService)
+	{
+        this.hotSearchService = hotSearchService;
+    }
+
+    @GetMapping("/list")
 	@Operation(summary = "获取热搜列表" , description = "获取热搜列表")
 	@Parameter(name = "shopId", description = "店铺id")
 	public ServerResponseEntity<List<HotSearchVO>> listByShopId(@RequestParam("shopId") Long shopId)
